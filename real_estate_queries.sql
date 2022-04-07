10a98f646e2686dd3e17d04beeae84e76fbc5971
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-07 15:01:01 -0500

    Finished lab

--- real_estate_queries.sql
+++ real_estate_queries.sql
@@ -1,22 +1,32 @@
 --Retrieve all info on the first 10 tenants (HINT: Use LIMIT)
+--SELECT * FROM tenants LIMIT 10;
 
 --Retrieve the name, age, and gender of the first 10 tenants
+--SELECT name, age, gender FROM tenants LIMIT 10;
 
 --Retrieve all info on all tenants older than 65
+--SELECT * FROM tenants WHERE age > 65;
 
 --Retrieve all info on all tenants in apartment with id 20
+--SELECT * FROM tenants JOIN apartments ON tenants.apartment_id = apartments.id WHERE apartments.id = 20;
 
 --Retrieve all info on all tenants in apartment with ids 20 or 21
+--SELECT * FROM tenants JOIN apartments ON tenants.apartment_id = apartments.id WHERE apartments.id = 20 OR apartments.id = 21;
 
 --Retrieve the names of all doormen and the address where they work
+--SELECT name, building_id FROM doormen;
 
 --Geriatric Birthday! Update all tenants whose age is 90 to be 91
+--UPDATE tenants SET age = 91 WHERE age = 90;
 
 --Delete all tenants whose age is greater than 65
+--DELETE FROM tenants WHERE age > 65;
 
 --Change all doormen from building 3 to work night shifts.
+--UPDATE doormen SET shift = 'Night' WHERE building_id = 3;
 
 --Create one new tenant, put them in any apartment you want
+--INSERT INTO tenants(name, age, gender, apartment_id) VALUES ('Daniel', 22, 'Male', 3);
 
 --Find just the ids for all apartments for building with id of 2
 
